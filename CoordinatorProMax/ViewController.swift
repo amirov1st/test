bf7add92aafdb29d481005c187f09d47443fa530
Amirov Foma <amirov.foma@gmail.com>
2024-12-15 21:52:17 +0300

    Initial Commit

---
+++ CoordinatorProMax/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoordinatorProMax
+//
+//  Created by Amirov Foma on 15.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
